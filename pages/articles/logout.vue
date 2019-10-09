6abc0152dabbbf65e5d61d9c1f0b2cb6b308b51c
Charlie <dussaultcharlie@gmail.com>
2019-10-09 10:30:26 +0200

    connexion

---
+++ pages/articles/logout.vue
@@ -0,0 +1,3 @@
+<template>
+    <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+</template>
---
+++ pages/fakeAccount.vue
@@ -0,0 +1,16 @@
+<template>
+  <p>{{ $store.state.login.pseudo}}</p>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+</style>
